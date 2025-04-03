149f6f9ab01411eb1ea030d5eda70563ad4389b3
Anup <anuppoudel60@gmail.com>
2025-04-03 11:37:33 +1100

    themes added

--- src/app/(main)/Navbar.tsx
+++ src/app/(main)/Navbar.tsx
@@ -4,8 +4,12 @@ import Link from "next/link";
 import logo from "@/assets/logo.png";
 import { UserButton } from "@clerk/nextjs";
 import { CreditCard } from "lucide-react";
+import ThemeToggle from "@/components/ThemeToggle";
+import { dark } from "@clerk/themes";
+import { useTheme } from "next-themes";
 
 export default function Navbar() {
+  const { theme } = useTheme();
   return (
     <header className="shadow-sm">
       <div className="g-3 mx-auto flex max-w-7xl items-center justify-between p-3">
@@ -21,19 +25,23 @@ export default function Navbar() {
             AI resume builder
           </span>
         </Link>
-        <UserButton
-          appearance={{
-            elements: { avatarBox: { width: 35, height: 35 } },
-          }}
-        >
-          <UserButton.MenuItems>
-            <UserButton.Link
-              label="Billing"
-              labelIcon={<CreditCard className="size-4" />}
-              href="/billing"
-            />
-          </UserButton.MenuItems>
-        </UserButton>
+        <div className="flex items-center gap-3">
+          <ThemeToggle />
+          <UserButton
+            appearance={{
+              baseTheme: theme === "dark" ? dark : undefined,
+              elements: { avatarBox: { width: 35, height: 35 } },
+            }}
+          >
+            <UserButton.MenuItems>
+              <UserButton.Link
+                label="Billing"
+                labelIcon={<CreditCard className="size-4" />}
+                href="/billing"
+              />
+            </UserButton.MenuItems>
+          </UserButton>
+        </div>
       </div>
     </header>
   );
