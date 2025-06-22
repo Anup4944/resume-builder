ab264ab0a92b16dbf242fea7493a1f1d40f4c9ac
Anup <anuppoudel60@gmail.com>
2025-06-22 21:32:36 +1000

    fetching and deleting resume feature added

--- src/app/(main)/resumes/page.tsx
+++ src/app/(main)/resumes/page.tsx
@@ -1,21 +1,56 @@
 import { Button } from "@/components/ui/button";
+import prisma from "@/lib/prisma";
+import { resumeDtInclude } from "@/lib/types";
+import { auth } from "@clerk/nextjs/server";
 import { PlusSquare } from "lucide-react";
 import { Metadata } from "next";
 import Link from "next/link";
+import ResumeItem from "./ResumeItem";
 
 export const metadata: Metadata = {
   title: "Your resumes",
 };
 
-export default function Page() {
+export default async function Page() {
+  const { userId } = await auth();
+
+  if (!userId) {
+    return null;
+  }
+
+  const [resumes, totalCount] = await Promise.all([
+    prisma.resume.findMany({
+      where: {
+        userId,
+      },
+      orderBy: {
+        updatedAt: "desc",
+      },
+      include: resumeDtInclude,
+    }),
+    prisma.resume.count({
+      where: { userId },
+    }),
+  ]);
+
+  //TODO: Check quato for non-premium users
+
   return (
-    <main className="mx-auto w-full max-w-7 space-y-6 px-3 py-6">
-      <Button asChild className="max-auto flex w-fit gap-2">
+    <main className="max-w-7x1 mx-auto w-full space-y-6 px-3 py-6">
+      <Button asChild className="mx-auto flex w-fit gap-2">
         <Link href="/editor">
           <PlusSquare className="size-5" />
           New Resume{" "}
         </Link>
       </Button>
+      <div className="space-y-1">
+        <h1 className="text-3xl font-bold">Your resumes ({totalCount})</h1>
+      </div>
+      <div className="flex w-full grid-cols-2 flex-col gap-3 sm:grid md:grid-cols-3 lg:grid-cols-4">
+        {resumes.map((resume) => (
+          <ResumeItem resume={resume} key={resume.id} />
+        ))}
+      </div>
     </main>
   );
 }
